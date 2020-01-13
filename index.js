c4b36ce8019ac6f2c3f302ea30a996471196b090
Oscar <mundonatural@gmail.com>
2020-01-13 14:29:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,27 @@ function whisper(string) {
 function logShout(string) {
   console.log(shout(string));
 }
+
+function logWhisper(string) {
+  console.log(whisper(string));
+}
+
+function sayHiToGrandma(string) {
+
+  var uppercase = "HELLO!"
+
+  if (string === 'I love you, Grandma.') {
+    return 'I love you, too.';
+  } else {
+    if(string.toUpperCase() === string) {
+      return 'YES INDEED!';
+    } else {
+        if(lowercase.toLowerCase() === lowercase) {
+          return 'I can\'t hear you!';
+        }
+    }
+  }
+
+  return 'Error';
+}
+
